adc637772aa2db07a4d897343d47ea095aa05839
Jeff Otuka <jeffotuka@gmail.com>
2019-10-22 00:58:35 -0300

    Project with Java 9 and Spring Boot 1.5.10

---
+++ myproject-web/src/main/java/br/com/mycompany/myproject/MyprojectApplication.java
@@ -0,0 +1,16 @@
+package br.com.mycompany.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application runner.
+ */
+@SpringBootApplication
+public class MyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyprojectApplication.class, args);
+	}
+
+}
