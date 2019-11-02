44cca7bc7a4b473a4c8b8abea7c17b4c1484d654
Jeff Otuka <jeffotuka@gmail.com>
2019-11-02 05:29:05 -0300

    Logger default instance

--- myproject-web/src/main/java/br/com/mycompany/myproject/startup/SystemConfiguration.java
+++ myproject-web/src/main/java/br/com/mycompany/myproject/startup/SystemConfiguration.java
@@ -2,8 +2,12 @@ package br.com.mycompany.myproject.startup;
 
 import br.com.mycompany.myproject.system.StandardSystemClock;
 import br.com.mycompany.myproject.system.SystemClock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 /**
  * Main configuration class of this application. Used for @Bean setup.
@@ -17,5 +21,11 @@ public class SystemConfiguration {
 	public SystemClock systemClock() {
 		return StandardSystemClock.systemUTC();
 	}
-}
 
+	@Bean
+	@Scope("prototype")
+	public Logger logger(InjectionPoint injectionPoint) {
+		Class<?> targetClass = injectionPoint.getMember().getDeclaringClass();
+		return LoggerFactory.getLogger(targetClass);
+	}
+}
